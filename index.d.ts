b2a1a99f31f6290b0632b67bd735f65afe8808ae
zergetaev <zergetaev@gmail.com>
2020-01-24 17:55:10 +0300

    Improve types.

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,35 @@
 declare module "react-app-location" {
   import React from "react";
-  import {Schema} from "yup";
+  import { Schema } from "yup";
   import { Link, Route } from "react-router-dom";
   import { LocationDescriptor } from "history";
 
-  type YupParams = Record<string, Schema>;
+  export type YupParams = Record<string, Schema>;
 
   export default class RouterLocation {
     constructor(
       path: string,
-      pathParamDefs: YupParams = {},
-      queryStringParamDefs: YupParams = {}
+      pathParamDefs: YupParams | null = {},
+      queryStringParamDefs: YupParams | null = {}
     );
 
     path: string;
 
     toRoute(
       renderOptions: { component?; render?; children?; invalid },
-      exact = false,
-      strict = false,
-      sensitive = false
+      exact: boolean = false,
+      strict: boolean = false,
+      sensitive: boolean = false
     ): Route;
 
-    toLink(
-      children: React.ReactNode,
-      params: YupParams,
-      props = {}
-    ): Link;
+    toLink(children: React.ReactNode, params: YupParams, props = {}): Link;
+
+    toUrl(params?: object): string;
 
     parseLocationParams(
-      location: LocationDescriptor = window.location,
+      location?: LocationDescriptor | Location,
       match?: object
-    ): object;
+    ): object | null;
 
     isValidParams(params?: object): boolean;
   }
